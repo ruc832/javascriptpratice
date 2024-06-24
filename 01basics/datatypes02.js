e75d6acb75740519676497ebebe52aadd17b2a95
ruc832 <chauhanruchi274@gmail.com>
2024-06-24 17:56:29 +0000

    var and datatypes

---
+++ 01basics/datatypes02.js
@@ -0,0 +1,6 @@
+let age=34
+let name="ruchi"
+let identity="true"
+console.log([age,name,identity]);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01basics/var01.js
@@ -0,0 +1,7 @@
+const accountId=123456
+let accountemail="ruchi@gmail.com"
+var accountpassword="56789"
+let accountstate;
+// accountId=5
+// console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountstate]);
